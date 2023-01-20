0f244193c38737dbf5fec2ad583a37efa40ee491
Minin Valentin <63961721+TeanWolf@users.noreply.github.com>
2023-01-20 18:46:08 +0500

    заменена

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.example.chatproject
 
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.chatproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //активация binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
+        setUpActionBar()
         //подключение к бд
         val database = Firebase.database
         //создание пробной записи в БД
@@ -26,6 +34,11 @@ class MainActivity : AppCompatActivity() {
         binding.buttonSend.setOnClickListener {
             myRef.setValue("Hello!")
         }
+    }
+    //создание выхода из приложения
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     //добавление сообщений на рабочий стол rview
@@ -45,4 +58,21 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
+    //добавление иконки и имени
+    private fun setUpActionBar()
+    {
+        val ab = supportActionBar
+        Thread{
+            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val dIcon = BitmapDrawable(resources, bMap)
+            runOnUiThread{
+                ab?.setDisplayHomeAsUpEnabled(true)
+                ab?.setHomeAsUpIndicator(dIcon)
+                ab?.title = auth.currentUser?.displayName
+            }
+        }.start()
+
+    }
+
 }
